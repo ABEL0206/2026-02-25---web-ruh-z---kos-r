48042813a3e9cd7a249f34463fb6186466c10ec7
ABEL0206 <wagnerabel95@gmail.com>
2026-02-25 08:32:49 +0100

    First commit

--- kod.js
+++ kod.js
@@ -1,8 +1,18 @@
 function nagyitKep(melyikKep) {
+    let nagyKep = document.getElementById("nagykep");
+    nagyKep.src = melyikKep;
 }
 
 function visszaKep() {
+    let nagyKep = document.getElementById("nagykep");
+    nagyKep.src = "ures.jpg";
 }
 
 function kivalaszt(melyikTermek) {
+    let termekSzama = window.prompt("Hány darabot szeretne: ");
+    let kosar = document.getElementById("kosar");
+    let ujElem = document.createElement("span");
+    ujElem.textContent = termekSzama+" db "+" "+melyikTermek.textContent;
+    kosar.append(ujElem);
+    kosar.append(document.createElement("br"));
 }
