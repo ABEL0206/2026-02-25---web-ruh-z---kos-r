f350eff7eb85cd35aa82d269f5c03b4af179d45e
wagnerabel <wagnerabel95@gmail.com>
2026-02-28 18:53:32 +0100

    Price upgrade 2026_02_28

--- kod.js
+++ kod.js
@@ -1,3 +1,11 @@
+const ram = 53850;
+const monitor = 70000;
+const hdd = 34615;
+const ssd = 51900;
+const psu = 18410;
+
+let totalPriceContainer = document.getElementById("total");
+let totalPrice = 0;
 function nagyitKep(melyikKep) {
     let nagyKep = document.getElementById("nagykep");
     nagyKep.src = melyikKep;
@@ -8,11 +16,32 @@ function visszaKep() {
     nagyKep.src = "ures.jpg";
 }
 
-function kivalaszt(melyikTermek) {
-    let termekSzama = window.prompt("Hány darabot szeretne? ");
-    let kosar = document.getElementById("kosar");
-    let ujElem = document.createElement("span");
-    ujElem.textContent = termekSzama+" db "+" "+melyikTermek.textContent;
-    kosar.append(ujElem);
-    kosar.append(document.createElement("br"));
+function arSzamitas(element){
+    let priceText = document.getElementById(element.id+"Price");
+    switch (element.id) {
+        case "ram":
+            priceText.value = ram * element.value +" FT";
+            totalPrice += parseInt(priceText.value, 10);
+            break;
+
+        case "monitor":
+            priceText.value = monitor * element.value + " FT";
+            totalPrice += parseInt(priceText.value, 10);
+            break;
+        
+        case "hdd":
+            priceText.value = hdd * element.value + " FT";
+            totalPrice += parseInt(priceText.value, 10);
+            break;
+
+        case "ssd":
+            priceText.value = ssd * element.value + " FT";
+            totalPrice += parseInt(priceText.value, 10);
+            break;
+        case "psu":
+            priceText.value = psu * element.value + " FT";
+            totalPrice += parseInt(priceText.value, 10);
+            break;
+    }
+    totalPriceContainer.innerText = totalPrice + " FT";
 }
